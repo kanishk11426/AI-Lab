0214e28009e0383544fd04654b736b468842d984
Kanishk V <kanishk11carmel@gmail.com>
2026-09-25 20:08:01 +0530

    ,

--- First question/script.js
+++ First question/script.js
@@ -7,10 +7,11 @@ function bill() {
     const puke = document.getElementById("output");
 
     console.log(units, rate);
-
+    const total = units * rate;
+    
     puke.innerHTML =`
         <h3>The no. of units are : ${units}</h3>
         <h3>The rate is : ${rate}</h3>
-        <h3>The no. of units are : ${units * rate}</h3>
+        <h3>The no. of units are : ${total}</h3>
     `
 };
